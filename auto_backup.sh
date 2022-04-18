6e802ec3f0570ab08c7a88ba45e8456155889f64
Ubuntu <ubuntu@ip-172-26-12-4.us-west-2.compute.internal>
2022-04-18 20:07:53 +0000

    regular backup

--- auto_backup.sh
+++ auto_backup.sh
@@ -1,3 +1,10 @@
+#Folder 
+SERVER_DIR_NAME="minecraft_server"
+GITHUB_USERNAME="MY_GITHUB_USERNAME"
+GITHUB_PERSONAL_TOKEN="MY_GITHUB_PERSONAL_TOKEN"
+BACKUP_INTERVAL="1m" #parameter use for sleep command. Here are some example: 2 (2 seconds), 2m (2 minute), 2h (2 hours), 2d (2 days)
+
+cd $SERVER_DIR_NAME
 while [ : ]
 do
     echo "-------------------Backup Start-------------------"
@@ -5,7 +12,7 @@ do
     git status
     git add .
     git commit -m "regular backup"
-    git push https://MineraftServer2536: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@github.com/MineraftServer2536/MinecraftServerBackups.git --all
+    git push https://$GITHUB_USERNAME:$GITHUB_PERSONAL_TOKEN@github.com/MineraftServer2536/MinecraftServerBackups.git --all
     echo "-------------------Backup Completed-------------------"
-    sleep 500
+    sleep $BACKUP_INTERVAL
 done
