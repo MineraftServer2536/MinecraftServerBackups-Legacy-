8f21f0da49e2cf11f6a307db29fcc54e126acb57
Ubuntu <ubuntu@ip-172-26-12-4.us-west-2.compute.internal>
2022-04-18 20:14:45 +0000

    update server auto back script

--- minecraft_server_auto_backup.sh
+++ minecraft_server_auto_backup.sh
@@ -1,8 +1,7 @@
-#Folder 
-SERVER_DIR_NAME="minecraft_server"
-GITHUB_USERNAME="MY_GITHUB_USERNAME"
+SERVER_DIR_NAME="minecraft_server" #Folder name of the minecraft server (will cd to this folder before calling git commands)
+GITHUB_USERNAME="MY_GITHUB_USERNAME" 
 GITHUB_PERSONAL_TOKEN="MY_GITHUB_PERSONAL_TOKEN"
-BACKUP_INTERVAL="1m" #parameter use for sleep command. Here are some example: 2 (2 seconds), 2m (2 minute), 2h (2 hours), 2d (2 days)
+BACKUP_INTERVAL="1m" #Parameter use for sleep command. Here are some examples: 2 (2 seconds), 2m (2 minute), 2h (2 hours), 2d (2 days)
 
 cd $SERVER_DIR_NAME
 while [ : ]
