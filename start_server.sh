394166ed3cf565c51a5c9356f4d6ff53623eb202
Ubuntu <ubuntu@ip-172-26-12-4.us-west-2.compute.internal>
2022-04-18 19:30:24 +0000

    initial commit

---
+++ start_server.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+while true
+do
+java -Xmx1024M -Xms1024M -jar server.jar nogui
+echo "If you want to completely stop the server process now, press Ctrl+C before
+the time is up!"
+echo "Rebooting in:"
+for i in 5 4 3 2 1
+do
+echo "$i..."
+sleep 1
+done
+echo "Rebooting now!"
+done 
